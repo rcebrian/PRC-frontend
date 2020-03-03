293e52dcb0ac40a108763cd5414d9ebc64134c94
Jazmin <21750959@live.uem.es>
2020-03-03 20:45:23 +0100

    Div resultado y lista de resultados terminado

--- src/app/components/analysis/analysis.component.ts
+++ src/app/components/analysis/analysis.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { AnalysisService } from 'src/app/analysis.service';
-import { HttpClient } from '@angular/common/http';
+import {AnalysisService} from 'src/app/analysis.service';
+import {HttpClient} from '@angular/common/http';
 
 export class Sentiment {
   polarity: Text;
@@ -15,24 +15,44 @@ export class Sentiment {
 })
 
 export class AnalysisComponent implements OnInit {
-
+  // Use for send data to python
   msg: Text;
   lib: number;
-  data: Text;
 
+  // Actual sentiment
   resultAnalysisData: Sentiment;
-  polarity: Text;
 
-  constructor(private analysisService: AnalysisService, private httpClient: HttpClient) { }
+  // Use for sentiment text result
+  msgSave: Text;
+  messageExist: boolean;
+  listResult: Array<string> = [];
+
+  constructor(private analysisService: AnalysisService, private httpClient: HttpClient) {
+  }
 
   ngOnInit(): void {
     this.lib = 0;
+    this.messageExist = false;
   }
 
   getAnalysis() {
+    if (this.messageExist) {
+      const cinco = this.msgSave + '\nPolarity data: ' + this.resultAnalysisData.polarity
+        + '\nSubjectivity data: ' + this.resultAnalysisData.subjectivity;
+      this.listResult.push(cinco);
+    }
+
+    // Get result of the sentiment analysis
     this.analysisService.getAnalysis(this.lib, this.msg).subscribe(
-      data => {this.resultAnalysisData = data as Sentiment;},
-      error => {console.log(this.resultAnalysisData); }
+      data => {
+        this.resultAnalysisData = data as Sentiment;
+      },
+      error => {
+        console.log(this.resultAnalysisData);
+      }
     );
+
+    this.msgSave = this.msg;
+    this.messageExist = true;
   }
 }
