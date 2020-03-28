42f50e4fca23183c842e383ea00d0fac3d9957e1
Jazmin <21750959@live.uem.es>
2020-03-28 14:45:17 +0100

    Desactivar boton hasta obtener respuesta
    He tocado analisis de sentimientos.

--- src/app/components/analysis/analysis.component.ts
+++ src/app/components/analysis/analysis.component.ts
@@ -29,6 +29,8 @@ export class AnalysisComponent implements OnInit {
   connection: boolean;
   interval;
 
+  buttonDisabled: boolean;
+  warningEmptyMsg: string;
 
   // Actual sentiment
   resultAnalysisData: Sentiment;
@@ -45,12 +47,14 @@ export class AnalysisComponent implements OnInit {
     this.lib = 1;
     this.connection = true;
     this.messageExist = false;
+    this.buttonDisabled = false;
     // @ts-ignore
     this.interval = setInterval(this.checkConnection.bind(this), 2000);
   }
 
   getAnalysis() {
-    if (this.messageExist) {
+    this.buttonDisabled = true;
+    if (this.messageExist && this.resultAnalysisData != null) {
       const sLog = {
         lib: this.lib,
         message: this.msgSave,
@@ -59,19 +63,28 @@ export class AnalysisComponent implements OnInit {
       } as SentimentLog;
       // Guarda los nuevos elementos al principio del array
       this.listResult.unshift(sLog);
+      this.resultAnalysisData = null;
     }
 
-    // Get result of the sentiment analysis
-    this.analysisService.getAnalysis(this.lib, this.msg).subscribe(
-      data => {
-        this.resultAnalysisData = data as Sentiment;
-      },
-      error => {
-        alert(`An error occurred with the server connection`);
-      }
-    );
-    this.msgSave = this.msg;
-    this.messageExist = true;
+    if (this.msg != null && this.msg.toString() !== '') {
+      this.warningEmptyMsg = null;
+      // Get result of the sentiment analysis
+      this.analysisService.getAnalysis(this.lib, this.msg).subscribe(
+        data => {
+          this.resultAnalysisData = data as Sentiment;
+          this.buttonDisabled = false;
+        },
+        error => {
+          alert(`An error occurred with the server connection`);
+          this.buttonDisabled = false;
+        }
+      );
+      this.msgSave = this.msg;
+      this.messageExist = true;
+    } else {
+      this.warningEmptyMsg = 'Enter a message';
+      this.buttonDisabled = false;
+    }
   }
 
   openDialog() {
