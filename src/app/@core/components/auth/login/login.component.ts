e46cf40d717ee9a0315c8a7b769e5db47656a2dc
rcebrian <rodrigocebriang@gmail.com>
2020-05-17 12:49:31 +0200

    login component without validators

--- src/app/@core/components/auth/login/login.component.ts
+++ src/app/@core/components/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {AuthService} from "../../../services/auth/auth.service";
+import {TokenStorageService} from "../../../services/auth/token-storage.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: any = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  email: string;
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.redirectToDashboard();
+    }
   }
+  onSubmit() {
+    this.authService.login(this.form).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+
+        console.log(JSON.stringify(data));
+        this.redirectToDashboard();
+
+        if (data.status === 401) {
+          this.isLoginFailed = true;
+        }
+
+      },
+      err => {
+        this.isLoginFailed = true;
+      }
+    );
+  }
+  redirectToDashboard(): void {
+    this.router.navigateByUrl('dashboard');
+
+  }
+
+  registerClick(): void {
+    this.router.navigateByUrl('register');
+  };
 
 }
